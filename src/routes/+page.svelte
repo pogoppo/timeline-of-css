0880a2d06fe0684272f8ec2f80464766dbe94ece
pogoppo <5822076+pogoppo@users.noreply.github.com>
2023-03-16 22:13:35 +0900

    ローディング演出の追加

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,12 +26,46 @@
 <main>
   {#if items}
     <Timeline {items} />
+  {:else}
+    <div class="centering" />
   {/if}
 </main>
 
 <style lang="scss">
+  @keyframes loading {
+    0% {
+      content: "   ";
+    }
+    25% {
+      content: ".  ";
+    }
+    50% {
+      content: ".. ";
+    }
+    75% {
+      content: "...";
+    }
+  }
   main {
     max-width: var(--contents-base-width);
     margin: 0 auto;
+  }
+  .centering {
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+    &::before {
+      content: "   ";
+      display: block;
+      animation: 2s linear 0s infinite normal loading;
+      white-space: pre-wrap;
+      font-size: 48px;
+      font-family: "Courier New", Courier, monospace;
+    }
   }
 </style>
