84c7e247d7ac4d6a2d6577769c116037809768ac
pogoppo <5822076+pogoppo@users.noreply.github.com>
2024-12-11 12:24:16 +0900

    データ取得関連のメンテナンス

--- scraping/bcd.ts
+++ scraping/bcd.ts
@@ -3,20 +3,7 @@ import bcd from '@mdn/browser-compat-data' assert { type: 'json' };
 import type { BrowserName, CompatStatement } from '@mdn/browser-compat-data';
 
 const prisma = new PrismaClient()
-
-async function create(prismaInstance: any, tablename: string, param: { data: any }) {
-  try {
-    await prismaInstance[tablename].create(param);
-  } catch (e) {
-    if (e instanceof Prisma.PrismaClientKnownRequestError) {
-      if (e.code === "P2002") {
-        // 重複エラーは無視
-        return;
-      }
-    }
-    console.error(e);
-  }
-}
+type PrismaInstance = Omit<PrismaClient<Prisma.PrismaClientOptions, never, any>, "$connect" | "$disconnect" | "$on" | "$transaction" | "$use" | "$extends">
 
 // https://zenn.dev/cohky/articles/prisma-to-truncate より
 async function truncate() {
@@ -29,81 +16,129 @@ async function truncate() {
   for (const modelName of modelNames) {
     const query = `DELETE FROM ${modelName}`;
     await prisma.$queryRawUnsafe(query);
+    console.log(`DELETE ${modelName}.`);
   };
 }
 
-async function createCSS(browser: BrowserName, prismaInstance: any) {
-  const createItem = async (
-    category: string,
-    name: string,
-    item: any,
-    parent: string,
-  ) => {
-    const compat = (item as any)['__compat'] as CompatStatement;
-    const support = compat.support[browser];
-    if (support === undefined) {
-      return;
-    }
+type CSSItem = {
+  name: string,
+  category: string,
+  parent: string,
+  browser: BrowserName,
+  version: number,
+  link: string | undefined,
+  description: string | undefined,
+}
+function createCSSItems(
+  browser: BrowserName,
+  category: string,
+  name: string,
+  item: any,
+  parent: string,
+) {
+  const compat = (item as any)['__compat'] as CompatStatement;
+  const support = compat.support[browser];
+  if (support === undefined) {
+    return [];
+  }
 
-    let version: number;
-    if (Array.isArray(support)) {
-      const versionString = String(support[support.length - 1]["version_added"]);
-      version = Number(versionString?.match(/(\d+)/)?.[0]);
-    } else {
-      const versionString = String(support["version_added"]);
-      version = Number(versionString?.match(/(\d+)/)?.[0]);
-    }
-    version = !!version ? version : 0;
+  let version: number;
+  if (Array.isArray(support)) {
+    const versionString = String(support[support.length - 1]["version_added"]);
+    version = Number(versionString?.match(/(\d+)/)?.[0]);
+  } else {
+    const versionString = String(support["version_added"]);
+    version = Number(versionString?.match(/(\d+)/)?.[0]);
+  }
+  version = !!version ? version : 0;
 
-    await create(prismaInstance, "css", {
-      data: {
-        name,
-        category,
-        parent,
-        browser,
-        version,
-        link: compat["mdn_url"],
-        description: compat["description"],
-      }
-    });
-    console.log(`Create ${category}.${name} for ${browser} version ${version}`);
+  const items: CSSItem[] = [];
+  items.push({
+    name,
+    category,
+    parent,
+    browser,
+    version,
+    link: compat["mdn_url"],
+    description: compat["description"],
+  });
+  console.log(`Create ${category}.${name} for ${browser} version ${version}`);
 
-    if (parent === name) {
-      const children = Object.keys(item).filter(k => k !== '__compat');
-      for (const childName of children) {
-        const parentName = name;
-        await createItem(category, childName, item[childName], parentName);
-      }
+  if (parent === name) {
+    const children = Object.keys(item).filter(k => k !== '__compat');
+    for (const childName of children) {
+      const parentName = name;
+      items.push(...createCSSItems(browser, category, childName, item[childName], parentName));
     }
   }
 
+  return items;
+}
+
+async function createCSS(browser: BrowserName, prismaInstance: PrismaInstance) {
+  const tablename = "css";
+  console.log(`Create ${tablename} table.`);
+
+  const createdItems: CSSItem[] = [];
   for (const category in bcd.css) {
     const items = Object.entries(bcd.css[category]);
     for (const [name, item] of items) {
-      await createItem(category, name, item, name);
+      const parent = name;
+      createdItems.push(...createCSSItems(browser, category, name, item, parent));
     }
   }
+
+  // skipDuplicatesがないので自前で実装
+  const uniqueItems = createdItems.filter((item, index, self) => {
+    return self.findIndex((t) => (
+      t.name === item.name &&
+      t.category === item.category &&
+      t.parent === item.parent &&
+      t.browser === item.browser
+    )) === index;
+  });
+
+  await prismaInstance[tablename].createMany({
+    data: uniqueItems
+  })
 }
 
-async function createVersions(browser: BrowserName, prismaInstance: any) {
+type Version = {
+  browser: BrowserName,
+  version: number,
+  release_date: Date,
+};
+async function createVersions(browser: BrowserName, prismaInstance: PrismaInstance) {
+  const tablename = "versions";
+  console.log(`Create ${tablename} table.`);
+
   const releases = bcd.browsers[browser]["releases"];
+  const versions: Version[] = [];
   for (const [version, item] of Object.entries(releases)) {
     const yyyymmdd = item["release_date"] ?? ""; // ソース元がYYYY-MM-DDのみで適当
-    await create(prismaInstance, "versions", {
-      data: {
-        browser,
-        version: Number(version),
-        release_date: new Date(yyyymmdd) // YYYY-MM-DDだけを引数とする場合はUTCで返すらしい
-      },
+    versions.push({
+      browser,
+      version: Number(version),
+      release_date: new Date(yyyymmdd) // YYYY-MM-DDだけを引数とする場合はUTCで返すらしい
     });
   }
+
+  await prismaInstance[tablename].createMany({
+    data: versions
+  })
 }
 
 async function main() {
   const browser = "firefox";
+
+  // SQLiteとPrismaの仕様上、トランザクション内でtruncateができない
+  console.log(`Beging truncate.`);
+  await truncate();
+
   return await prisma.$transaction(async (prismaInstance) => {
-    await truncate();
+    console.log(`Beging createCSS.`);
     await createCSS(browser, prismaInstance);
+    console.log(`Beging createVersions.`);
     return createVersions(browser, prismaInstance);
   });
 }
